5dc62b2d253ee6da508a57f695c68f3cce0ff808
mdj982 <maodejun001@hotmail.com>
2020-03-13 15:10:43 +0900

    vscode configs

---
+++ vscode/readme.md
@@ -0,0 +1,23 @@
+#### supported version
+1.43.0
+
+#### run setall.sh
+```bash
+bash setall.sh
+```
+
+#### location: settings.json
+Linux:  
+~/.config/Code/User/settings.json  
+Windows:  
+%APPDATA%\Code\User\settings.json  
+WSL:  
+${PWD}/Code/User/settings.json  
+
+#### location: keybindings.json
+Linux:  
+~/.config/Code/User/keybindings.json  
+Windows:  
+%APPDATA%\Code\User\keybindings.json  
+WSL:  
+${PWD}/Code/User/keybindings.json  
---
+++ vscode/setall.sh
@@ -0,0 +1,9 @@
+cp settings.json ~/.config/Code/User/settings.json
+cp keybindings.json ~/.config/Code/User/keybindings.json
+code --install-extension ms-vscode.cpptools --force
+code --install-extension torn4dom4n.latex-support --force
+code --install-extension james-yu.latex-workshop --force
+code --install-extension ms-dotnettools.csharp --force
+code --install-extension gimly81.matlab --force
+code --install-extension koehlma.markdown-math --force
+
