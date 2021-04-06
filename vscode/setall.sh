c6d872a5988c3d67bc83acaf41b4f1a191ea7b02
mdj982 <maodejun001@hotmail.com>
2021-04-06 14:14:29 +0900

    add vscode extension remote ssh

--- vscode/setall.sh
+++ vscode/setall.sh
@@ -9,6 +9,7 @@ code --install-extension ms-dotnettools.csharp --force
 code --install-extension gimly81.matlab --force
 code --install-extension koehlma.markdown-math --force
 code --install-extension ms-python.python --force
+code --install-extension ms-vscode-remote.remote-ssh --force
 # code --install-extension  --force
 # code --install-extension  --force
 # code --install-extension  --force
