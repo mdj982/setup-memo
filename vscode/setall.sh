b10ddf9b7af078ab513e9116dace6a0cb51c3955
mdj982 <maodejun001@hotmail.com>
2021-10-22 13:19:46 +0900

    update markdown extension

--- vscode/setall.sh
+++ vscode/setall.sh
@@ -7,7 +7,7 @@ code --install-extension torn4dom4n.latex-support --force
 code --install-extension james-yu.latex-workshop --force
 code --install-extension ms-dotnettools.csharp --force
 code --install-extension gimly81.matlab --force
-code --install-extension koehlma.markdown-math --force
+code --install-extension yzhang.markdown-all-in-one --force
 code --install-extension ms-python.python --force
 code --install-extension ms-vscode-remote.remote-ssh --force
 code --install-extension mechatroner.rainbow-csv --force
